c27125d0315bd1236b4f34a38315407c586bf269
jin hye <sjrry112@gmail.com>
2022-10-18 15:14:39 +0900

    221018_1st_Commit

---
+++ component/MyComponent.jsx
@@ -0,0 +1,9 @@
+// 2022.10.12.(수)
+// npm으로 react프로젝트를 작성할 때 import해서 사용
+// import {Component} from 'react'; // 
+
+class MyComponent extends React.Component{
+    render(){
+        return <h1>클래스형 컴포넌트 모듈입니다</h1> //
+    }
+}
